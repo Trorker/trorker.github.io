d3a73d79553f69d473b99e13421ecef5cd7e957e
Ruslan Dzyuba <ruslick.ua@gmail.com>
2024-03-03 03:20:41 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,207 @@
+let code = `
+                &lt;!DOCTYPE&nbsp;HTML&gt;<br>
+                &lt;html&nbsp;lang="it"&gt;<br>
+                <br>
+                &lt;head&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&lt;meta&nbsp;name="viewport"&nbsp;content="width=device-width,&nbsp;initial-scale=1"&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&lt;title&gt;Work&nbsp;in&nbsp;Progress&lt;/title&gt;<br>
+                <br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&lt;link&nbsp;rel="icon"&nbsp;href="🚧"&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&lt;link&nbsp;rel="stylesheet"&nbsp;href="styles.css"&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&lt;link&nbsp;rel="preload"&nbsp;href="/background.jpg"&nbsp;as="image"&nbsp;/&gt;<br>
+                &lt;/head&gt;<br>
+                <br>
+                &lt;body&nbsp;class="bg&nbsp;noselect"&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&lt;section&nbsp;class="content"&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;hgroup&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;h1&gt;The&nbsp;Site&nbsp;is&nbsp;Under&nbsp;Construction&lt;/h1&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;p&gt;Stay&nbsp;tuned&nbsp;for&nbsp;updates&nbsp;and&nbsp;come&nbsp;back&nbsp;soon&nbsp;to&nbsp;see&nbsp;the&nbsp;magic&nbsp;unfold!&lt;/p&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;/hgroup&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;div&nbsp;class="code"&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;span&nbsp;class="type"&nbsp;style="--n:239"&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;!DOCTYPE&nbsp;html&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;html&nbsp;lang="en"&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;head&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;meta&nbsp;charset="UTF-8"&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;meta&nbsp;name="viewport"&nbsp;content="width=device-width,&nbsp;initial-scale=1.0"&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;title&gt;Work&nbsp;in&nbsp;Progress...&lt;/title&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;/head&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;body&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;h1&gt;Work&nbsp;in&nbsp;Progress&lt;/h1&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;/body&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;/html&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;/span&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;/div&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&lt;/section&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&lt;footer&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;span&nbsp;class="credits"&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;div&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Made&nbsp;by&nbsp;&lt;a&nbsp;href="https://github.com/Trorker"&nbsp;class="secondary"&gt;Ruslan&nbsp;Dzyuba&lt;/a&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;/div&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;div&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Copyright&nbsp;©&nbsp;2024&nbsp;-&nbsp;All&nbsp;rights&nbsp;reserved<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;/div&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;/span&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;span&nbsp;class="unsplash"&gt;Photo&nbsp;by&nbsp;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;a&nbsp;href="https://unsplash.com/it/@dianamia?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"&gt;C&nbsp;Dustin&lt;/a&gt;&nbsp;from&nbsp;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;a&nbsp;href="https://unsplash.com/it/foto/un-gruppo-di-edifici-alti-sotto-un-cielo-blu-nuvoloso-91AQt9p4Mo8?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"&gt;Unsplash&lt;/a&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;/span&gt;<br>
+                &nbsp;&nbsp;&nbsp;&nbsp;&lt;/footer&gt;<br>
+                &lt;/body&gt;<br>
+                <br>
+                &lt;/html&gt;
+`;
+
+
+var app = document.getElementById('codeType');
+let newCod = `
+<!DOCTYPE HTML>
+<html lang="it">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Work in Progress</title>
+
+    <link rel="icon"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚧</text></svg>">
+
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Portfolio Ruslan Dzyuba">
+    <meta property="og:description" content="Work in Progress...">
+    <meta property="og:url" content="https://www.ruslan-dzyuba.it/">
+    <meta property="og:image"
+        content="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚧</text></svg>">
+
+    <link rel="stylesheet" href="styles.css">
+
+    <link rel="preload" href="/background-tiny.jpg" as="image" />
+
+    <script src="https://unpkg.com/typewriter-effect@latest/dist/core.js"></script>
+</head>
+
+<body class="bg noselect">
+    <section class="content">
+        <hgroup>
+            <h1>The Site is Under Construction</h1>
+            <p>Stay tuned for updates and come back soon to see the magic unfold!</p>
+        </hgroup>
+        <div class="code">
+            <span id="codeType" class="type"></span>
+        </div>
+    </section>
+    <footer>
+        <span class="credits">
+            <div>
+                Made by <a href="https://github.com/Trorker" class="secondary">Ruslan Dzyuba</a>
+            </div>
+            <div>
+                Copyright © 2024 - All rights reserved
+            </div>
+        </span>
+        <span class="unsplash">Photo by <a
+                href="https://unsplash.com/it/@dianamia?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">C
+                Dustin</a> from <a
+                href="https://unsplash.com/it/foto/un-gruppo-di-edifici-alti-sotto-un-cielo-blu-nuvoloso-91AQt9p4Mo8?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+        </span>
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
+`;
+
+
+function Typewriter(element, speed, options) {
+    this.element = element;
+    this.speed = speed || 100; // Velocità predefinita di scrittura (in millisecondi)
+    this.options = options || {}; // Altre opzioni, come ad esempio il testo da scrivere
+
+    this.init = function() {
+        if (!this.element) return; // Verifica che sia stato fornito un elemento valido
+
+        var text = this.options.text || ''; // Ottiene il testo da scrivere, se fornito
+        var index = 0;
+        var self = this;
+
+        // Cancella il contenuto dell'elemento
+        this.element.innerHTML = '';
+
+        // Funzione ricorsiva per scrivere il testo lettera per lettera
+        function typeWriter() {
+            if (index < text.length) {
+
+                console.log(text.charAt(index) + ": ", text.charCodeAt(index));
+                console.log();
+                
+                switch (text.charCodeAt(index)) {
+                    case 10:
+                        self.element.innerHTML += "<br>"
+                        break;
+                    case 32:
+                        self.element.innerHTML += "&nbsp;"
+                        break;
+        
+                    default:
+                        self.element.innerHTML += text.charAt(index);
+                        break;
+                }
+                index++;
+                setTimeout(typeWriter, self.speed);
+            }
+        }
+
+        // Avvia la scrittura del testo
+        typeWriter();
+    }
+}
+
+// Esempio di utilizzo
+var typewriter = new Typewriter(app, 50, { text: newCod });
+typewriter.init();
+
+
+/*var i = 0;
+var speed = 50;
+
+typeWriter();
+
+function typeWriter() {
+    if (i < newCod.length) {
+        console.log(newCod.charAt(i) + ": ", newCod.charCodeAt(i));
+        console.log();
+        switch (newCod.charCodeAt(i)) {
+            case 10:
+                app.innerHTML += "<br>"
+                break;
+            case 32:
+                app.innerHTML += "&nbsp;"
+                break;
+
+            default:
+                app.innerHTML += newCod.charAt(i);
+                break;
+        }
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}*/
+
+/*
+var typewriter = new Typewriter(app, {
+    loop: true,
+    cursor: "|",
+    devMode: true
+});
+
+typewriter.typeString(newCod).start();*/
+
+
+/*.pauseFor(2500)
+.deleteAll()
+.typeString('Strings can be removed')
+.pauseFor(2500)
+.deleteChars(7)
+.typeString('<strong>altered!</strong>')
+.pauseFor(2500)
+.start();*/
